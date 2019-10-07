be16cfb310b849eee08c3d619499b0fad56b0d7e
gnoy21 <pmy0237@naver.com>
2019-10-07 15:25:17 +0900

    test

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 function postClick(test){
-    location.href="post/"+test+".html"
+    location.href="post/"+test+".html";
+    var fs = require('fs');
+    var files = fs.readdirSync('../post/');
+    alert(files);
 }
